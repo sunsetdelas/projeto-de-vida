692713ba70999111c94d03dcb1b8946535bb9422
sunsetdelas <138136444+sunsetdelas@users.noreply.github.com>
2025-05-30 09:12:19 -0300

    arquivo js criado

---
+++ main.js
@@ -0,0 +1,15 @@
+const botoes = document. querySelectorAll(".botao");
+const botoes = document. querySelectorAll(".aba-conteudo");
+
+for(let i=0; i< botoes.length; j++) {
+    botoes[i].onclick = function () { 
+
+for (let j =0; j <botoes.length; j++){
+      botoes[j].classList.romove("ativo");
+      textos[j].classList.remove("ativo");
+ }
+  
+    botoes[j].classList.romove("ativo");
+    textos[j].classList.remove("ativo");
+}
+} 
